d557e31c3251e23e8bd8eb603ddbeab83cd7da01
komugi8 <8448eito@gmail.com>
2025-01-02 18:44:21 +0900

    サイドバーのUI修正

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,12 +9,25 @@ const Sidebar = ({ onTypeClick }) => {
     "隣接集合が同じ頂点を一つの頂点として扱う手法",
     "頂点順序に応じたアニメーションをつける手法",
     "力学モデルによるレイアウトの改善",
-    "二部置換グラフ",
+    // "二部置換グラフ",
   ];
+  const bipartiteTypes = ["二部置換グラフ"];
   return (
     <div className="sidebar">
       <div className="sidebar-list">
+        <div className="sidebar-title">真区間グラフ</div>
         {types.map((type) => (
+          <p
+            key={type}
+            className="sidebar-item"
+            onClick={() => onTypeClick(type)}
+          >
+            {type}
+          </p>
+        ))}
+        <div className="sidebar-section"></div>
+        <div className="sidebar-title">二部置換グラフ</div>
+        {bipartiteTypes.map((type) => (
           <p
             key={type}
             className="sidebar-item"
