dcb70f25b63f31dc59bf26d95240528402989717
komugi8 <8448eito@gmail.com>
2025-01-23 01:35:00 +0900

    曲線対応交差数計算

--- src/components/SimpleDrawing.js
+++ src/components/SimpleDrawing.js
@@ -40,6 +40,9 @@ const CytoscapeComponent = forwardRef(({ binaryValue }, ref) => {
           selector: "edge.nonSequential",
           style: {
             "curve-style": "unbundled-bezier",
+            // "control-point-distance": 100, // 制御点の距離を設定
+            // "control-point-weight": 0.5, // 制御点の重みを設定
+            "control-point-step-size": 100, // 制御点のステップサイズを設定
           },
         },
       ],
@@ -96,7 +99,49 @@ const CytoscapeComponent = forwardRef(({ binaryValue }, ref) => {
       rows: 1,
     }).run();
 
+    function edgesIntersect(s1, t1, s2, t2) {
+      // 交差判定のロジックを実装
+      return (
+        (s1.x < s2.x && s2.x < t1.x && t1.x < t2.x) ||
+        (s1.x === s2.x &&
+          t1.x < t2.x &&
+          t1.x - s1.x > 80 &&
+          t2.x - s2.x > 80) ||
+        (t1.x === t2.x && s1.x < s2.x && t1.x - s1.x > 80 && t2.x - s2.x > 80)
+      );
+      //   let s = (s1.x - t1.x) * (s2.y - s1.y) - (s1.y - t1.y) * (s2.x - s1.x);
+      //   let t = (s1.x - t1.x) * (t2.y - s1.y) - (s1.y - t1.y) * (t2.x - s1.x);
+      //   let u = (s2.x - t2.x) * (s1.y - s2.y) - (s2.y - t2.y) * (s1.x - s2.x);
+      //   let v = (s2.x - t2.x) * (t1.y - s2.y) - (s2.y - t2.y) * (t1.x - s2.x);
+      //   return s * t < 0 && u * v < 0;
+    }
+
+    function updateIntersectionCount() {
+      let intersectionCount = 0;
+
+      edges.forEach((edge1, i) => {
+        const s1 = cy.getElementById(edge1.data.source).position();
+        const t1 = cy.getElementById(edge1.data.target).position();
+        edges.slice(i + 1).forEach((edge2) => {
+          const s2 = cy.getElementById(edge2.data.source).position();
+          const t2 = cy.getElementById(edge2.data.target).position();
+          if (edgesIntersect(s1, t1, s2, t2)) {
+            intersectionCount++;
+            console.log(`Edge1: ${s1.x} -> ${t1.x}`);
+            console.log(`Edge2: ${s2.x} -> ${t2.x}`);
+          }
+        });
+      });
+
+      console.log(`Edge Intersections: ${intersectionCount}`);
+    }
+
+    cy.on("position", "node", updateIntersectionCount);
+
+    updateIntersectionCount();
+
     return () => {
+      cy.off("position", "node", updateIntersectionCount);
       cy.destroy();
     };
   }, [binaryValue]);
